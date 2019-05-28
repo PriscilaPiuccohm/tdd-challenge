4714ae51c07b9093b79e39bd196636c35410e248
brunoas <brunoas@ciasc.sc.gov.br>
2019-05-28 14:10:52 -0300

    Resposta

--- src/test/java/exerciciosBasicos/exercicio28.java
+++ src/test/java/exerciciosBasicos/exercicio28.java
@@ -1,4 +1,20 @@
 package exerciciosBasicos;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class exercicio28 {
+
+    @Test
+    public void contandoLetras() {
+
+
+        String nome  = "Priscila";
+
+        List<String> nomes = Arrays.asList("p", "r", "i", "s", "c", "i", "l", "a");
+
+
+    }
 }
